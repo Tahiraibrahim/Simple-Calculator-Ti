b54f8fbdd8f11939a2b647d49e1c918a5442c3cd
Tahira Ibrahim <tahirasalahuddin77@gmail.com>
2024-04-29 13:50:15 +0500

    first commit

---
+++ tical-simple-calculator-by-ti.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const ans = await inquirer.prompt([{
+    type: "number",
+    name: "first_number",
+    message: "Enter your First Number: ",
+},
+{
+    type: "number",
+    name: "second_number",
+    message: "Enter your Second Number: ",
+},
+{
+    type: "list",
+    name: "operator",
+    message: "Please select operation type: ",
+    choices: ["Additio" , "Subtraction" , "Multiplication" , "Division"]
+}
+])
+
+if(ans.operator === "Addition"){
+    console.log(ans.first_number + ans.second_number);
+}
+
+if(ans.operator === "Subtraction"){
+    console.log(ans.first_number - ans.second_number);
+}
+
+if(ans.operator === "Multiplication"){
+    console.log(ans.first_number * ans.second_number);
+}
+
+else{
+    console.log(ans.first_number / ans.second_number);
+}
